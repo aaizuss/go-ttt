ed70381a5cf53bb3be1c6203c6e9e5b0ac632b56
aaizuss <aaizuss@gmail.com>
2018-01-28 20:24:50 -0800

    Make a Board struct and add methods for marking a space and checking if a space is marked

--- board.go
+++ board.go
@@ -2,15 +2,36 @@ package board
 
 const EmptySpace string = "_"
 
-func New(size int) []string {
-	board := make([]string, size*size)
-	for i := range board {
-		board[i] = EmptySpace
+type Board struct {
+	spaces []string
+}
+
+func (board Board) Spaces() []string {
+	return board.spaces
+}
+
+func (board Board) NumSpaces() int {
+	return len(board.spaces)
+}
+
+func (board Board) GetSpace(index int) string {
+	return board.spaces[index]
+}
+
+func New(dimension int) Board {
+	board := Board{}
+
+	board.spaces = make([]string, dimension*dimension)
+	for i := range board.spaces {
+		board.spaces[i] = EmptySpace
 	}
 	return board
 }
 
-func MarkSpace(board []string, space int, marker string) []string {
-	board[space] = marker
-	return board
+func (board Board) IsMarked(index int) bool {
+	return board.spaces[index] != EmptySpace
+}
+
+func (board Board) MarkSpace(index int, marker string) {
+	board.spaces[index] = marker
 }
--- board_test.go
+++ board_test.go
@@ -6,29 +6,49 @@ import (
 )
 
 func TestNewBoardReturnsBoardFromDimensions(t *testing.T) {
-	board := board.New(3)
+	blankBoard := board.New(3)
 
-	if len(board) != 9 {
-		t.Errorf("Wanted length 9, got %d", len(board))
+	if blankBoard.NumSpaces() != 9 {
+		t.Errorf("Expected length 9, got %d", blankBoard.NumSpaces)
 	}
 }
 
 func TestNewBoardReturnsEmptyBoard(t *testing.T) {
 	blankBoard := board.New(3)
 
-	for i := 0; i < len(blankBoard); i++ {
-		if blankBoard[i] != board.EmptySpace {
-			t.Errorf("Expected board[%d] to be '_', got %q", i, blankBoard[i])
+	for i := range blankBoard.Spaces() {
+		if blankBoard.IsMarked(i) {
+			t.Errorf("Expected board[%d] to be '_', got %q", i, blankBoard.GetSpace(i))
 		}
 	}
 }
 
+func TestIsMarkedReturnsTrueWhenSpaceIsMarked(t *testing.T) {
+	board := board.New(3)
+
+	board.MarkSpace(4, "x")
+
+	if !board.IsMarked(4) {
+		t.Errorf("Expected IsMarked(4) to be true, got false")
+	}
+}
+
+func TestIsMarkedReturnsFalseWhenSpaceIsEmpty(t *testing.T) {
+	board := board.New(3)
+
+	board.MarkSpace(4, "x")
+
+	if board.IsMarked(3) {
+		t.Errorf("Expected IsMarked(3) to be false, got true")
+	}
+}
+
 func TestMarkSpace(t *testing.T) {
-	blankBoard := board.New(3)
+	myboard := board.New(3)
 
-	result := board.MarkSpace(blankBoard, 6, "x")
+	myboard.MarkSpace(6, "x")
 
-	if result[6] != "x" {
-		t.Errorf("Expected space 6 to be x, got %q", result[6])
+	if myboard.GetSpace(6) != "x" {
+		t.Errorf("Expected space 6 to be x, got %q", myboard.GetSpace(6))
 	}
 }
