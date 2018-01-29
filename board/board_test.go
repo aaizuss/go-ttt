45683fbb25f029a790720410bb8565b7eec3145b
aaizuss <aaizuss@gmail.com>
2018-01-28 21:12:58 -0800

    Create board directory for board package

---
+++ board/board_test.go
@@ -0,0 +1,54 @@
+package board_test
+
+import (
+	"github.com/aaizuss/tictactoe/board"
+	"testing"
+)
+
+func TestNewBoardReturnsBoardFromDimensions(t *testing.T) {
+	blankBoard := board.New(3)
+
+	if blankBoard.NumSpaces() != 9 {
+		t.Errorf("Expected length 9, got %d", blankBoard.NumSpaces)
+	}
+}
+
+func TestNewBoardReturnsEmptyBoard(t *testing.T) {
+	blankBoard := board.New(3)
+
+	for i := range blankBoard.Spaces() {
+		if blankBoard.IsMarked(i) {
+			t.Errorf("Expected board[%d] to be '_', got %q", i, blankBoard.GetSpace(i))
+		}
+	}
+}
+
+func TestIsMarkedReturnsTrueWhenSpaceIsMarked(t *testing.T) {
+	board := board.New(3)
+
+	board.MarkSpace(4, "x")
+
+	if !board.IsMarked(4) {
+		t.Errorf("Expected IsMarked(4) to be true, got false")
+	}
+}
+
+func TestIsMarkedReturnsFalseWhenSpaceIsEmpty(t *testing.T) {
+	board := board.New(3)
+
+	board.MarkSpace(4, "x")
+
+	if board.IsMarked(3) {
+		t.Errorf("Expected IsMarked(3) to be false, got true")
+	}
+}
+
+func TestMarkSpace(t *testing.T) {
+	myboard := board.New(3)
+
+	myboard.MarkSpace(6, "x")
+
+	if myboard.GetSpace(6) != "x" {
+		t.Errorf("Expected space 6 to be x, got %q", myboard.GetSpace(6))
+	}
+}
