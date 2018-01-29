6fbcffaefd06ffb9c71975e343345a2fdd8a332e
aaizuss <aaizuss@gmail.com>
2018-01-29 15:25:13 -0800

    Create files for formatting the board as a string

---
+++ board/formatter.go
@@ -0,0 +1,17 @@
+package board
+
+// func (board Board) formatRow()
+
+// space marker space divider space marker space divider space marker\n
+//
+//  0 | 1 | x
+// -----------
+//  3 | x | 5
+// -----------
+//  6 | 7 | 8
+//
+//  0 | 1 | x
+// ---+---+---
+//  3 | x | 5
+// ---+---+---
+//  6 | 7 | 8
---
+++ board/formatter_test.go
@@ -0,0 +1,17 @@
+package board
+
+// import (
+// 	"testing"
+// )
+
+//
+// func TestFormatRow(t *testing.T) {
+// 	board := New(3)
+// 	expected := " 0 | 1 | 2\n"
+//
+// 	result := board.formatRow()
+//
+// 	if result != true {
+// 		t.Errorf("Expected true, got %v", result)
+// 	}
+// }
