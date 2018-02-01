2770f1ec31cc671b8e4311f704fe4e8e4d7304f0
aaizuss <aaizuss@gmail.com>
2018-01-31 20:20:01 -0800

    Test ai v ai

--- game/game_test.go
+++ game/game_test.go
@@ -76,8 +76,23 @@ func TestTakeTurnsHaltsWhenThereIsAWinner(t *testing.T) {
 	}
 }
 
+func TestComputersAgainstEachOtherResultsInTie(t *testing.T) {
+	cli := MockConsole{}
+	p1 := Player{marker: "x", isHuman: false}
+	p2 := Player{marker: "o", isHuman: false}
+	players := []Player{p1, p2}
+	game := Game{board: board.New(3), players: players, ui: &cli}
+
+	game.takeTurns()
+	result := game.board.IsTie()
+	expect := true
+
+	if result != expect {
+		t.Errorf("Expected %v, got %v", expect, result)
+	}
+}
+
 func almostTieBoard() board.Board {
-	// x will move to 8 to tie
 	board := board.New(3)
 
 	board.MarkSpace(0, "x")
